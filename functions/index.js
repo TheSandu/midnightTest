9a5338b8584482d59d95b0011d0817e870537959
TheSandu <thesandu97@gmail.com>
2021-03-09 00:45:42 +0200

    Added userData service

--- functions/index.js
+++ functions/index.js
@@ -16,19 +16,19 @@ const db = admin.firestore();
 exports.register = functions.https.onRequest(async (request, response) => {
 
     if( request.method !== "POST") {
-        response.status(403).send('Error: Bad request');
+        response.status(403).send('RegisterService Error: Bad request');
         return;
     }
 
     let { username, password, name, email } = request.body;
 
     if( username === undefined || password === undefined || name === undefined || email === undefined ) {
-        response.status(403).send('Error: No data');
+        response.status(403).send('RegisterService Error: No data');
         return;
     }
 
     if( !validateEmail( email ) ) {
-        response.status(403).send('Error: Bad email');
+        response.status(403).send('RegisterService Error: Bad email');
         return;
     }
 
@@ -40,9 +40,31 @@ exports.register = functions.https.onRequest(async (request, response) => {
     });
 
     if( !userRef.id ) {
-        response.status(403).send('Error: User not added');
+        response.status(403).send('RegisterService Error: User not added');
         return;
     }
 
     response.status(500).send( `Success: UserId ${userRef.id}` );
+});
+
+
+// Can be some issues with functions becouse of proxy server, can test it fast sorry
+exports.userData = functions.https.onRequest(async (request, response) => {
+
+    const ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
+
+    if( !ip ) {
+        response.status(403).send('UserDataService Error: Ip undefined');
+        return;
+    }
+
+    const userInfo = await fetch(`https://geo.ipify.org/api/v1?apiKey=${config.ipifyAPIKey}&ipAddress=${ip}`);
+    const data = await userInfo.json();
+
+    if( data.hasOwnProperty('code') && data.code !== 500 ) {
+        response.status(403).send(`UserDataService Error: ${data.messages}`);
+        return;
+    }
+
+    response.status(500).json( data );
 });
